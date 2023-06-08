c8f2f8d5e413c18b20ae1ac0b792d3ee2d93635c
laxmanPal <laxi27902@gmail.com>
2023-06-08 08:50:18 +0530

    added mobile menu

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import "./style.css";
+
+const mobileMenu = document.querySelector(".burger-menu");
+const navigation = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
