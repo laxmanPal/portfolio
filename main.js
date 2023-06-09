ebb9ef7c0b0bc9193b314300447f910d15b73a71
laxmanPal <laxi27902@gmail.com>
2023-06-09 09:15:26 +0530

    added hero section

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-import "./style.css";
+import "./styles/modern-normalize.css";
+import "./styles/style.css";
 
-const mobileMenu = document.querySelector(".burger-menu");
-const navigation = document.querySelector(".navigation");
+// const mobileMenu = document.querySelector(".burger-menu");
+// const navigation = document.querySelector(".navigation");
 
-mobileMenu.addEventListener("click", () => {
-  mobileMenu.classList.toggle("active");
-  navigation.classList.toggle("active");
-});
+// mobileMenu.addEventListener("click", () => {
+//   mobileMenu.classList.toggle("active");
+//   navigation.classList.toggle("active");
+// });
+
+// gsap.to(".overflow-hidden", {
+//   y: 0,
+//   duration: 0.2,
+//   stagger: 0.1,
+// });
