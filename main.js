ffce93f414a922540963cd4f5b275a1345925dda
laxmanPal <laxi27902@gmail.com>
2023-06-11 16:38:54 +0530

    added preloader

--- main.js
+++ main.js
@@ -1,48 +1,7 @@
 import "./styles/modern-normalize.css";
 import "./styles/style.css";
 
-// const mobileMenu = document.querySelector(".burger-menu");
-// const navigation = document.querySelector(".navigation");
-
-// mobileMenu.addEventListener("click", () => {
-//   mobileMenu.classList.toggle("active");
-//   navigation.classList.toggle("active");
-// });
-
-// gsap.to(".overflow-hidden", {
-//   y: 0,
-//   duration: 0.2,
-//   stagger: 0.1,
-// });
-
-// let innerCursor = document.querySelector(".inner-cursor");
-// let outerCursor = document.querySelector(".outer-cursor");
-
-// document.addEventListener("mousemove", moveCursor);
-
-// function moveCursor(e) {
-//   let x = e.clientX;
-//   let y = e.clientY;
-//   innerCursor.style.left = `${x}px`;
-//   innerCursor.style.top = `${y}px`;
-//   setTimeout(() => {
-//     outerCursor.style.left = `${x}px`;
-//     outerCursor.style.top = `${y}px`;
-//   }, 100);
-// }
-
-// let links = Array.from(document.querySelectorAll("nav-links"));
-
-// links.forEach((link) => {
-//   link.addEventListener("mouseover", () => {
-//     innerCursor.classList.add("grow");
-//     outerCursor.style.display = "none";
-//   });
-//   link.addEventListener("mouseleave", () => {
-//     innerCursor.classList.remove("grow");
-//     outerCursor.style.display = "block";
-//   });
-// });
+import gsap from "gsap";
 
 let text = document.querySelector(".text p");
 
@@ -53,9 +12,113 @@ text.innerHTML = text.innerHTML
   })
   .join("");
 
-let circleImg = document.querySelector(".circle-wrapper");
-let backdrop = document.querySelector(".backdrop");
+// let circleImg = document.querySelector(".circle-wrapper");
+// let backdrop = document.querySelector(".backdrop");
 
-circleImg.addEventListener("click", () => {
-  backdrop.classList.toggle("active");
+// circleImg.addEventListener("click", () => {
+//   // backdrop.classList.toggle("active");
+
+//   gsap.to(backdrop, {
+//     width: "100%",
+//     height: "100vh",
+//     display: "flex",
+//     alignItem: "center",
+//     justifyContent: "center",
+//     duration: 0.6,
+//   });
+// });
+
+// backdrop.addEventListener("click", () => {
+//   backdrop.classList.remove("active");
+// });
+
+// circleImg.addEventListener("click", () => {
+//   gsap.to([".header ,.heading-huge "], {
+//     display: "none",
+//     duration: 0.6,
+//   });
+
+//   gsap.to(circleImg, {
+//     width: "100%",
+//     height: "100vh",
+//     display: "flex",
+//     alignItem: "center",
+//     justifyContent: "center",
+//     delay: 0.5,
+//     duration: 0.6,
+//   });
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const tl = gsap.timeline();
+
+  tl.from("#first", {
+    onStart: () => {
+      $("#first").textillate({
+        in: {
+          effect: "fadeInUp",
+          callback: () => {
+            $("#first").textillate("out");
+          },
+        },
+        out: { effect: "fadeOutUp" },
+      });
+    },
+  })
+    .from("#second", {
+      opacity: 0,
+      delay: 1.1,
+      onStart: () => {
+        $("#second").textillate({
+          in: {
+            effect: "fadeInUp",
+            callback: () => {
+              $("#second").textillate("out");
+            },
+          },
+          out: { effect: "fadeOutUp" },
+        });
+      },
+    })
+    .from("#third", {
+      opacity: 0,
+      delay: 1.1,
+      onStart: () => {
+        $("#third").textillate({
+          in: {
+            effect: "fadeInUp",
+            callback: () => {
+              $("#third").textillate("out");
+            },
+          },
+          out: { effect: "fadeOutUp" },
+        });
+      },
+    })
+    .from("#fourth", {
+      opacity: 0,
+      delay: 1.1,
+      onStart: () => {
+        $("#fourth").textillate({
+          in: {
+            effect: "fadeInUp",
+            callback: () => {
+              $("#fourth").textillate("out");
+            },
+          },
+          out: { effect: "fadeOutUp" },
+        });
+      },
+    })
+    .to(".preloader-container", {
+      top: "-100%",
+      delay: 1,
+      duration: 1,
+      ease: "Power4.out",
+    })
+    .to(".hero-background", {
+      width: "100%",
+      delay: 0.5,
+      duration: 1,
+    });
 });
